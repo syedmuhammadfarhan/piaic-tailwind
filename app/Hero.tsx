f8d24a45f57006e0451ed4d9710d640084113f32
syedmuhammadfarhan <114781442+syedmuhammadfarhan@users.noreply.github.com>
2023-04-02 04:19:20 +0500

    2nd commit

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -3,23 +3,29 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="flex">
-      <div className="w-1/3">
+    <div className="flex items-center">
+      <div className="w-2/4 m-10">
         <Image src="/president.png" alt="arif alvi" width={300} height={300} />
       </div>
       <div className="bg-slate-300 p-10">
-        <h1 className="max-w-full w-2/3 text-5xl font-bold">
+
+        <h1 className="max-w-full w-2/4 text-5xl font-bold mb-2">
           Presidential Initiative
         </h1>
-        <h1 className="text-4xl">for Artificial Intelligence & Computing</h1>
-        <p className="p-58">
-          The mission of PIAIC is to reshape Pakistan by revolutionizing
-          education, research, and business by adopting latest, cutting-edge
-          technologies. Experts are calling this the 4th industrial revolution.
-          We want Pakistan to become a global hub for AI, data science, cloud
-          native computing, edge computing, blockchain, augmented reality, and
-          internet of things.
-        </p>
+        <div className="text-4xl mb-2">
+
+          <h1 >for Artificial Intelligence & Computing</h1>
+        </div>
+        <div className="pr-52 text-justify leading-7">
+          <p >
+            The mission of PIAIC is to reshape Pakistan by revolutionizing
+            education, research, and business by adopting latest, cutting-edge
+            technologies. Experts are calling this the 4th industrial revolution.
+            We want Pakistan to become a global hub for AI, data science, cloud
+            native computing, edge computing, blockchain, augmented reality, and
+            internet of things.
+          </p>
+        </div>
       </div>
     </div>
   );
--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,18 +3,18 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div>
-      <div className="flex bg-slate-400 justify-between">
-        <div className="flex gap-40">
-          <Image src="/PIAICLogo.webp" alt="piaiclogo" width={50} height={50}/>
-          <ul className="flex gap-10">
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-        <>Apply</>
+
+    <div className="flex bg-slate-400 justify-between items-center pt-3">
+      <div className="flex gap-10">
+        <Image className="ml-5 -mb-11" src="/PIAICLogo.webp" alt="piaiclogo" width={70} height={70} />
+        <ul className="flex gap-10 items-center">
+          <li><a href="/">Home</a></li>
+          <li><a href="/">About us</a></li>
+          <li><a href="/">Contact Us</a></li>
+        </ul>
       </div>
+      <button className="bg-emerald-600 m-2 mr-4 px-3.5 py-1.5 rounded-3xl text-white">Apply</button>
     </div>
+
   );
 }
