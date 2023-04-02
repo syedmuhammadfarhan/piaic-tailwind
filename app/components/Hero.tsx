00985b0014d86d4675ad1feca9fd8927f81b7dbd
syedmuhammadfarhan <114781442+syedmuhammadfarhan@users.noreply.github.com>
2023-04-02 23:29:32 +0500

    mob nav

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="sm:flex items-center">
-      <div className="md:w-1/3 m-10 ml-20">
+      <div className="md:w-1/3 m-10 ml-12">
         <Image src="/president.png" alt="arif alvi" width={300} height={300} />
       </div>
-      <div className="w-2/3 bg-slate-300 p-10 text-justify">
+      <div className="w-2/3 bg-slate-300 p-5 text-justify">
         <h1 className="text-5xl font-bold mb-2">Presidential Initiative</h1>
         <div className="text-4xl mb-2">
           <h1>for Artificial Intelligence & Computing</h1>
