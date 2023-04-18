894bd92ee279b933a1f39c8eb0f8083c41a1151a
syedmuhammadfarhan <114781442+syedmuhammadfarhan@users.noreply.github.com>
2023-04-18 20:49:17 +0500

    after q3and4

--- app/Data/Quarters-data.tsx
+++ app/Data/Quarters-data.tsx
@@ -1,3 +1,36 @@
+export const progBanner = {
+  wmdBanner: {
+    heading: "Web 3.0 (Blockchain) and Metaverse Specialization",
+    description:
+      "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.",
+  },
+  aiBanner: {
+    heading: "Artificial Intelligence (AI) and Deep Learning Specialization",
+    description:
+      "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+  },
+  cncBanner: {
+    heading: "Cloud-Native Computing Specialization",
+    description:
+      "The Cloud-Native Computing Specialization focuses on Containers, Kubernetes, and CDK for Kubernetes.",
+  },
+  iotBanner: {
+    heading: "Ambient Computing and IoT Specialization",
+    description:
+      "The Ambient Computing and IoT Specialization focuses on building Smart Homes, Offices, Factories, and Cities using Voice computing, Matter Protocol, and Embedded Devices.",
+  },
+  bioBanner: {
+    heading: "Genomics and Bioinformatics Specialization",
+    description:
+      "Genomics is the study of the total genetic makeup of individual organisms, and how this genetic information is structured, functions, and has evolved; bioinformatics encompasses a diverse range of analytical methods and tools applied to genomic data. This Specialization focuses on performing complex bioinformatics analysis using the most essential Python libraries and applications.",
+  },
+  npaBanner: {
+    heading: "Network Programmability and Automation Specialization",
+    description:
+      "More than ever, network engineers are finding it challenging to complete their duties entirely manually. Network automation is now crucial due to new protocols, technologies, delivery models, and the requirement for enterprises to become more adaptable and agile. This course teaches network engineers how to automate systems with code using a variety of technologies and tools, including Linux, Python, APIs, and Git.",
+  },
+};
+
 export const CommonQuarters = [
   {
     heading: "Qaurter I",
@@ -17,7 +50,7 @@ export const CommonQuarters = [
   },
 ];
 
-export const WmdQuarters = [
+export const wmdQuarters = [
   {
     heading: "Quarter IV",
     description:
@@ -30,7 +63,7 @@ export const WmdQuarters = [
   },
 ];
 
-export const AiQuarters = [
+export const aiQuarters = [
   {
     heading: "Quarter IV",
     description:
--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,40 +1,16 @@
 import Image from "next/image";
 
-import { CommonQuarters, WmdQuarters } from "../Data/Quarters-data";
+import {
+  CommonQuarters,
+  aiQuarters,
+  bioQuarters,
+  cncQuarters,
+  iotQuarters,
+  npaQuarters,
+  progBanner,
+  wmdQuarters,
+} from "../Data/Quarters-data";
 import Link from "next/link";
-
-const progBanner = {
-  wmdBanner: {
-    heading: "Web 3.0 (Blockchain) and Metaverse Specialization",
-    description:
-      "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.",
-  },
-  aiBanner: {
-    heading: "Artificial Intelligence (AI) and Deep Learning Specialization",
-    description:
-      "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
-  },
-  cncBanner: {
-    heading: "Cloud-Native Computing Specialization",
-    description:
-      "The Cloud-Native Computing Specialization focuses on Containers, Kubernetes, and CDK for Kubernetes.",
-  },
-  iotBanner: {
-    heading: "Ambient Computing and IoT Specialization",
-    description:
-      "The Ambient Computing and IoT Specialization focuses on building Smart Homes, Offices, Factories, and Cities using Voice computing, Matter Protocol, and Embedded Devices.",
-  },
-  bioBanner: {
-    heading: "Genomics and Bioinformatics Specialization",
-    description:
-      "Genomics is the study of the total genetic makeup of individual organisms, and how this genetic information is structured, functions, and has evolved; bioinformatics encompasses a diverse range of analytical methods and tools applied to genomic data. This Specialization focuses on performing complex bioinformatics analysis using the most essential Python libraries and applications.",
-  },
-  npaBanner: {
-    heading: "Network Programmability and Automation Specialization",
-    description:
-      "More than ever, network engineers are finding it challenging to complete their duties entirely manually. Network automation is now crucial due to new protocols, technologies, delivery models, and the requirement for enterprises to become more adaptable and agile. This course teaches network engineers how to automate systems with code using a variety of technologies and tools, including Linux, Python, APIs, and Git.",
-  },
-};
 
 export default function AvailableProgramsD({
   params,
@@ -43,8 +19,6 @@ export default function AvailableProgramsD({
 }) {
   return (
     <div>
-      {" "}
-      my name is {params.name}
       <div className="md:flex justify-between h-1/3 bg-gradient-to-r from-blue-400 to-green-300 px-8 pb-6">
         {params.name === "wmd" && (
           <div className="md:w-2/3 py-10 md:py-20 md:pl-28">
@@ -156,25 +130,126 @@ export default function AvailableProgramsD({
           one or more specializations consisting of two courses each.
         </p>
         <div className="max-w-fit sm:flex-wrap md:flex justify-center gap-3 mt-8 mb-16">
-          {WmdQuarters.map((items, i) => (
-            <div
-              key={i}
-              className="border-2 rounded-3xl w-56 h-auto bg-gradient-to-r from-blue-400 to-green-300 mb-4"
-            >
-              <div className="flex items-center justify-center w-full">
-                <div className="flex h-40 w-40 rounded-full border-2 overflow-hidden mt-3 justify-center items-center text-lg font-bold bg-white">
-                  {items.heading}
+          {params.name === "wmd" &&
+            wmdQuarters.map((items, i) => (
+              <div
+                key={i}
+                className="border-2 rounded-3xl w-56 h-auto bg-gradient-to-r from-blue-400 to-green-300 mb-4"
+              >
+                <div className="flex items-center justify-center w-full">
+                  <div className="flex h-40 w-40 rounded-full border-2 overflow-hidden mt-3 justify-center items-center text-lg font-bold bg-white">
+                    {items.heading}
+                  </div>
+                </div>
+                <div className="flex h-36 border-white text-sm  text-white justify-center items-center border-b-2 mt-4 mx-4">
+                  {items.description}
+                </div>
+
+                <div className="text-sm text-slate-500 md:animate-pulse hover:scale-110 m-2">
+                  Duration: 3 Weeks
                 </div>
               </div>
-              <div className="flex h-36 border-white text-sm  text-white justify-center items-center border-b-2 mt-4 mx-4">
-                {items.description}
-              </div>
+            ))}
+          {params.name === "ai" &&
+            aiQuarters.map((items, i) => (
+              <div
+                key={i}
+                className="border-2 rounded-3xl w-56 h-auto bg-gradient-to-r from-blue-400 to-green-300 mb-4"
+              >
+                <div className="flex items-center justify-center w-full">
+                  <div className="flex h-40 w-40 rounded-full border-2 overflow-hidden mt-3 justify-center items-center text-lg font-bold bg-white">
+                    {items.heading}
+                  </div>
+                </div>
+                <div className="flex h-36 border-white text-sm  text-white justify-center items-center border-b-2 mt-4 mx-4">
+                  {items.description}
+                </div>
 
-              <div className="text-sm text-slate-500 md:animate-pulse hover:scale-110 m-2">
-                Duration: 3 Weeks
+                <div className="text-sm text-slate-500 md:animate-pulse hover:scale-110 m-2">
+                  Duration: 3 Weeks
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          {params.name === "cnc" &&
+            cncQuarters.map((items, i) => (
+              <div
+                key={i}
+                className="border-2 rounded-3xl w-56 h-auto bg-gradient-to-r from-blue-400 to-green-300 mb-4"
+              >
+                <div className="flex items-center justify-center w-full">
+                  <div className="flex h-40 w-40 rounded-full border-2 overflow-hidden mt-3 justify-center items-center text-lg font-bold bg-white">
+                    {items.heading}
+                  </div>
+                </div>
+                <div className="flex h-36 border-white text-sm  text-white justify-center items-center border-b-2 mt-4 mx-4">
+                  {items.description}
+                </div>
+
+                <div className="text-sm text-slate-500 md:animate-pulse hover:scale-110 m-2">
+                  Duration: 3 Weeks
+                </div>
+              </div>
+            ))}
+          {params.name === "iot" &&
+            iotQuarters.map((items, i) => (
+              <div
+                key={i}
+                className="border-2 rounded-3xl w-56 h-auto bg-gradient-to-r from-blue-400 to-green-300 mb-4"
+              >
+                <div className="flex items-center justify-center w-full">
+                  <div className="flex h-40 w-40 rounded-full border-2 overflow-hidden mt-3 justify-center items-center text-lg font-bold bg-white">
+                    {items.heading}
+                  </div>
+                </div>
+                <div className="flex h-36 border-white text-sm  text-white justify-center items-center border-b-2 mt-4 mx-4">
+                  {items.description}
+                </div>
+
+                <div className="text-sm text-slate-500 md:animate-pulse hover:scale-110 m-2">
+                  Duration: 3 Weeks
+                </div>
+              </div>
+            ))}
+          {params.name === "bio" &&
+            bioQuarters.map((items, i) => (
+              <div
+                key={i}
+                className="border-2 rounded-3xl w-56 h-auto bg-gradient-to-r from-blue-400 to-green-300 mb-4"
+              >
+                <div className="flex items-center justify-center w-full">
+                  <div className="flex h-40 w-40 rounded-full border-2 overflow-hidden mt-3 justify-center items-center text-lg font-bold bg-white">
+                    {items.heading}
+                  </div>
+                </div>
+                <div className="flex h-36 border-white text-sm  text-white justify-center items-center border-b-2 mt-4 mx-4">
+                  {items.description}
+                </div>
+
+                <div className="text-sm text-slate-500 md:animate-pulse hover:scale-110 m-2">
+                  Duration: 3 Weeks
+                </div>
+              </div>
+            ))}
+          {params.name === "npa" &&
+            npaQuarters.map((items, i) => (
+              <div
+                key={i}
+                className="border-2 rounded-3xl w-56 h-auto bg-gradient-to-r from-blue-400 to-green-300 mb-4"
+              >
+                <div className="flex items-center justify-center w-full">
+                  <div className="flex h-40 w-40 rounded-full border-2 overflow-hidden mt-3 justify-center items-center text-lg font-bold bg-white">
+                    {items.heading}
+                  </div>
+                </div>
+                <div className="flex h-36 border-white text-sm  text-white justify-center items-center border-b-2 mt-4 mx-4">
+                  {items.description}
+                </div>
+
+                <div className="text-sm text-slate-500 md:animate-pulse hover:scale-110 m-2">
+                  Duration: 3 Weeks
+                </div>
+              </div>
+            ))}
         </div>
       </center>
       <div className="text-xs md:text-sm text-slate-500 m-4 flex justify-center items-center gap-1">
