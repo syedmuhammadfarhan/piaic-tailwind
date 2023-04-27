3fc24087a877339edef535034c341b514fccb395
syedmuhammadfarhan <farhanpiaic@gmail.com>
2023-04-27 09:59:05 +0500

    readjust

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,31 +10,34 @@ export default function Hero() {
         <Image src="/president.png" alt="arif alvi" width={300} height={300} />
       </div>
       {/* 2nd div */}
-      <div className="md:basis-3/4 p-7">
-        <h2 className="text-4xl font-bold mb-2 text-center md:text-start">
+      <div className="md:basis-3/4 p-2">
+        <h2 className="text-xl md:text-4xl mb-1 text-center md:text-start font-extrabold pt-4">
           Presidential Initiative
         </h2>
 
-        <h3 className="text-3xl mb-2 text-center md:text-start">
+        <h3 className="text-lg md:text-3xl mb-1 text-center md:text-start font-">
           for Artificial Intelligence & Computing
         </h3>
 
-        <div className="md:pr-4 lg:pr-52 leading-6 text-justify text-sm text-black">
+        <div className="md:pr-4 lg:pr-52 leading-6 text-justify text-xs md:text-sm text-white">
+          <p className="">"</p>
           <p>
             The mission of PIAIC is to reshape Pakistan by revolutionizing
             education, research, and business by adopting latest, cutting-edge
             technologies. Experts are calling this the 4th industrial
             revolution. We want Pakistan to become a global hub for AI, data
             science, cloud native computing, edge computing, blockchain,
-            augmented reality, and internet of things.
+            augmented reality, and internet of things."
           </p>
         </div>
         <div className="pt-10 flex justify-end">
           <div className="bg-green-200 max-w-fit rounded-2xl p-2 shadow-inner shadow-black">
-            <p className=" text-3xl font-bold text-black md:animate-pulse">
+            <p className="text-xl md:text-3xl font-bold text-black md:animate-pulse text-center">
               100 K+
             </p>
-            <p className="text-[9px] text-slate-500">Applications Received</p>
+            <p className="text-[7px] md:text-[9px] text-slate-600 text-center">
+              Applications Received
+            </p>
           </div>
         </div>
       </div>
